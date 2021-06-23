b9a4d95f45e824da9d3efdd330c4b49115ac0886
pandewahyu <80252648+pandewahyu@users.noreply.github.com>
2021-06-23 19:56:42 +0800

    Add files via upload

---
+++ laravel/app/Http/Controllers/ProductApiController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductApiController extends Controller
+{
+    public function index(){
+        $products = Product::all();
+        return response()->json(['message' => 'Success', 'data' => $products]);
+    }
+
+    public function show($id){
+        $product = Product::find($id);
+        return response()->json(['message' => 'Success', 'data' => $product]);
+    }
+
+    public function store(Request $request){
+        $product = Product::create($request->all());
+        return response()->json(['message' => 'Berhasil Insert Data', 'data' => $product]);
+    }
+
+    public function update(Request $request, $id){
+        $product = Product::find($id);
+        $product->update($request->all());
+        return response()->json(['message' => 'Success update', 'data' => $product]);
+    }
+
+    public function destroy($id){
+        $product = Product::find($id);
+        $product->delete();
+        return response()->json(['message' => 'Success delete', 'data' => null]);
+    }
+}
